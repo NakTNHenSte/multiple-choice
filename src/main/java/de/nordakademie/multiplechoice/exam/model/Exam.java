46c8203b5c0200ae4ccb011ffb867250a47ca3d0
Daniel Fiolka <daniel.fiolka@nordakademie.de>
2017-11-07 18:07:32 +0100

    Lecturer java angelegt und exam klasse attribute hinzugefuegt

--- src/main/java/de/nordakademie/multiplechoice/exam/model/Exam.java
+++ src/main/java/de/nordakademie/multiplechoice/exam/model/Exam.java
@@ -1,7 +1,108 @@
 package de.nordakademie.multiplechoice.exam.model;
 
-/**
- * Created by Steven on 07.11.2017.
- */
+import de.nordakademie.multiplechoice.lecturer.model.Lecturer;
+
+import javax.persistence.*;
+import java.sql.Date;
+import java.sql.Time;
+
+
+@Entity
+@Table(name = "EXAM")
 public class Exam {
+
+    private long id;
+    private String title;
+    private Time duration;
+    private Short percentualSuccessThreshold;
+    private Date start;
+    private Date end;
+    private Byte credits;
+    private String seminar;
+    private Lecturer lecturer;
+
+    public Exam() {
+    }
+
+    public Exam(final long id, final String title, Lecturer lecturer) {
+        this.id = id;
+        this.title = title;
+        this.lecturer = lecturer;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long getId() {
+        return id;
+    }
+
+    public void setId(final long id) {
+        this.id = id;
+    }
+
+    @Basic
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(final String title) {
+        this.title = title;
+    }
+
+    @Basic
+    public Lecturer getLecturer() {
+        return lecturer;
+    }
+
+    public void setLecturer(Lecturer lecturer) {
+        this.lecturer = lecturer;
+    }
+    @Basic
+    public String getSeminar() {
+        return seminar;
+    }
+
+    public void setSeminar(String seminar) {
+        this.seminar = seminar;
+    }
+    @Basic
+    public Byte getCredits() {
+        return credits;
+    }
+
+    public void setCredits(Byte credits) {
+        this.credits = credits;
+    }
+    @Basic
+    public Date getEnd() {
+        return end;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
+    }
+    @Basic
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+    @Basic
+    public Short getPercentualSuccessThreshold() {
+        return percentualSuccessThreshold;
+    }
+
+    public void setPercentualSuccessThreshold(Short percentualSuccessThreshold) {
+        this.percentualSuccessThreshold = percentualSuccessThreshold;
+    }
+    @Basic
+    public Time getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Time duration) {
+        this.duration = duration;
+    }
 }
---
+++ src/main/java/de/nordakademie/multiplechoice/lecturer/model/Lecturer.java
@@ -0,0 +1,4 @@
+package de.nordakademie.multiplechoice.lecturer.model;
+
+public class Lecturer {
+}
