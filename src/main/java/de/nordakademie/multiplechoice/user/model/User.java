353d63ca6c58bbbe74ac99124d5549c8ffb51eb0
DieLue <dieke_morten.luebberstedt@nordakademie.de>
2017-11-07 21:59:31 +0100

    Änderungen an Model User

--- src/main/java/de/nordakademie/multiplechoice/user/model/User.java
+++ src/main/java/de/nordakademie/multiplechoice/user/model/User.java
@@ -1,4 +1,11 @@
 package de.nordakademie.multiplechoice.user.model;
 
 public class User {
+
+    private long id;
+    private String name;
+    private String surname;
+    private String username;
+    private String role;
+    private String password;
 }
