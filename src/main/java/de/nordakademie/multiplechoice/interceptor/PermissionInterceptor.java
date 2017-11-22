39d07298147d1edc6d1685979c53e31602149bc2
DieLue <dieke_morten.luebberstedt@nordakademie.de>
2017-11-22 22:42:12 +0100

    Interception eingebaut, ob der Anwender ein Dozent ist und die Seite angezeigt bekommen darf

---
+++ src/main/java/de/nordakademie/multiplechoice/interceptor/PermissionInterceptor.java
@@ -0,0 +1,37 @@
+package de.nordakademie.multiplechoice.interceptor;
+
+import java.util.Map;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+import de.nordakademie.multiplechoice.user.model.User;
+import de.nordakademie.multiplechoice.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+public class PermissionInterceptor extends AbstractInterceptor {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public String intercept(ActionInvocation invocation) throws Exception {
+        Map<String, Object> sessionMap = ActionContext.getContext().getSession();
+
+        if (sessionMap.containsKey("userId")) {
+
+            final long userId = (long) sessionMap.get("userId");
+            final User user = userService.find(userId);
+
+            if (user.getTyp().equals("D")) {
+
+                return invocation.invoke();
+
+            } else {
+                return "permissionRedirect";
+            }
+        }
+        return invocation.invoke();
+    }
+}
