dd819fd4a08896d92d5be440a2a2935e1959070c
smblr <blood_metal@gmx.net>
2017-11-26 00:44:55 +0100

    Anlegen von TestAnswer und redirect von saveTestAnswers zurück zu der frage

---
+++ src/main/java/de/nordakademie/multiplechoice/testAnswer/model/TestAnswer.java
@@ -0,0 +1,81 @@
+package de.nordakademie.multiplechoice.testAnswer.model;
+
+/**
+ * Created by Daniel Fiolka on 25.11.2017
+ */
+
+import de.nordakademie.multiplechoice.answer.model.Answer;
+import de.nordakademie.multiplechoice.exam.model.Exam;
+import de.nordakademie.multiplechoice.question.model.Question;
+import de.nordakademie.multiplechoice.user.model.User;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "testAnswer")
+public class TestAnswer {
+
+    private long id;
+    private String studentAnswer;
+    private Exam exam;
+    private User user;
+    private Answer answer;
+    private boolean correctness;
+
+    public TestAnswer() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long getId() {
+        return id;
+    }
+
+    public void setId(final long id) {
+        this.id = id;
+    }
+
+    public String getStudentAnswer() {
+        return studentAnswer;
+    }
+
+    public void setStudentAnswer(String studentAnswer) {
+        this.studentAnswer = studentAnswer;
+    }
+
+    @ManyToOne(optional = false)
+    public Exam getExam() {
+        return exam;
+    }
+
+    public void setExam(Exam exam) {
+        this.exam = exam;
+    }
+
+    @OneToOne
+    public Answer getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(Answer answer) {
+        this.answer = answer;
+    }
+
+    @OneToOne
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public boolean isCorrectness() {
+        return correctness;
+    }
+
+    public void setCorrectness(boolean correctness) {
+        this.correctness = correctness;
+    }
+}
+
