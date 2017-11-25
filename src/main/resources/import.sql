50c721f3d0516180608fd605fa41c17f64144e27
NakTNHenSte <steven.hens@nordakademie.de>
2017-11-26 00:09:42 +0100

    Import.sql angepasst

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,40 +1,40 @@
-INSERT INTO USER VALUES (1234, 'Hans', 'password', 'Petersen', 'S', 'hans.petersen');
-INSERT INTO USER VALUES (1235, 'Diecke', 'password', 'Luebberstadt', 'S', 'diecke.luebberstedt');
-INSERT INTO USER VALUES (1236, 'Chris', 'password', 'West', 'S', 'christ.west');
-INSERT INTO USER VALUES (1237, 'Schtiewn', 'password', 'Hens', 'S', 'schtiewn.hens');
-INSERT INTO USER VALUES (1238, 'Daniela', 'password', 'Katzenburger', 'S', 'daniela.katzenburger');
+INSERT INTO USER VALUES (123400, 'Hans', 'password', 'Petersen', 'S', 'hans.petersen');
+INSERT INTO USER VALUES (123500, 'Diecke', 'password', 'Luebberstadt', 'S', 'diecke.luebberstedt');
+INSERT INTO USER VALUES (123600, 'Chris', 'password', 'West', 'S', 'christ.west');
+INSERT INTO USER VALUES (123700, 'Schtiewn', 'password', 'Hens', 'S', 'schtiewn.hens');
+INSERT INTO USER VALUES (123800, 'Daniela', 'password', 'Katzenburger', 'S', 'daniela.katzenburger');
 
-INSERT INTO USER VALUES (4321, 'Uwe', 'nimda', 'Mayer', 'D', 'uwe.mayer');
-INSERT INTO USER VALUES (1000, 'Karl', '', 'Karlson', 'D', '');
+INSERT INTO USER VALUES (432100, 'Uwe', 'nimda', 'Mayer', 'D', 'uwe.mayer');
+INSERT INTO USER VALUES (100000, 'Karl', '', 'Karlson', 'D', '');
 
-INSERT INTO EXAM VALUES (1234, 1, 30, '2017-12-12', 30, 'Allgemeine Betriebswirtschaftslehre', '2017-09-17','Pruefung zum Seminar',4321);
-INSERT INTO EXAM VALUES (1235, 0.5, 30, '2017-09-30', 40, 'Controlling', '2017-10-10','Pruefung zum Seminar',4321);
-INSERT INTO EXAM VALUES (1236, 2, 30, '2017-10-31', 30, 'Wissenschaftliches Arbeiten', '2017-10-10','Pruefung zum Seminar',4321);
-INSERT INTO EXAM VALUES (1237, 4, 60, '2017-10-31', 30, 'Selbstbewusst fuehren', '2017-10-10','BWL',4321);
-INSERT INTO EXAM VALUES (1238, 2, 90, '2017-10-31', 30, 'Malen mit Zahlen', '2017-10-10','CSS & HTML',4321);
+INSERT INTO EXAM VALUES (123400, 1, 30, '2017-12-12', 30, 'Allgemeine Betriebswirtschaftslehre', '2017-09-17','Pruefung zum Seminar',432100);
+INSERT INTO EXAM VALUES (123500, 0.5, 30, '2017-09-30', 40, 'Controlling', '2017-10-10','Pruefung zum Seminar',432100);
+INSERT INTO EXAM VALUES (123600, 2, 30, '2017-10-31', 30, 'Wissenschaftliches Arbeiten', '2017-10-10','Pruefung zum Seminar',432100);
+INSERT INTO EXAM VALUES (123700, 4, 60, '2017-10-31', 30, 'Selbstbewusst fuehren', '2017-10-10','BWL',432100);
+INSERT INTO EXAM VALUES (123800, 2, 90, '2017-10-31', 30, 'Malen mit Zahlen', '2017-10-10','CSS & HTML',432100);
 
-INSERT INTO QUESTION VALUES (1001, 'Hier steht eine Frage', 'multiple', 1, -0.5, 0,  1234);
-INSERT INTO QUESTION VALUES (1002, 'Die ist die _%richtige%_ Antwort.', 'gap', 4, -1, 0,  1234);
-INSERT INTO QUESTION VALUES (1003, 'Wirtschaftsinformatik ist die Kombination aus _%Betriebswirtschaftslehre%_ und _%Informatik%_.', 'gap', 6, 0, 0,  1234);
-INSERT INTO QUESTION VALUES (1004, 'Wie heisst die Hauptstadt von China?', 'multiple', 1, 0, 0,  1234);
-INSERT INTO QUESTION VALUES (1005, 'Was kann ein Wirtschaftsinformatiker alles nach seiner Ausbildung?', 'multiple,' 1, -1, -0.5,  1234);
+INSERT INTO QUESTION VALUES (100100, 'Hier steht eine Frage', 'multiple', 1, -0.5, 0,  123400);
+INSERT INTO QUESTION VALUES (100200, 'Die ist die _%richtige%_ Antwort.', 'gap', 4, -1, 0,  123400);
+INSERT INTO QUESTION VALUES (100300, 'Wirtschaftsinformatik ist die Kombination aus _%Betriebswirtschaftslehre%_ und _%Informatik%_.', 'gap', 6, 0, 0,  123400);
+INSERT INTO QUESTION VALUES (100400, 'Wie heisst die Hauptstadt von China?', 'multiple', 1, 0, 0,  123400);
+INSERT INTO QUESTION VALUES (100500, 'Was kann ein Wirtschaftsinformatiker alles nach seiner Ausbildung?', 'multiple,' 1, -1, -0.5,  123400);
 
-INSERT INTO ANSWER VALUES (100, 'Softwaretechnik existiert seit 1969.', 1, 'false', 1001);
-INSERT INTO ANSWER VALUES (200, 'Das ist die richtige Antwort.', 2, 'true', 1001);
-INSERT INTO ANSWER VALUES (300, 'Das ist die falsche Antwort.', 3, 'true', 1001);
-INSERT INTO ANSWER VALUES (400, 'richtige', 1, 'true', 1002);
-INSERT INTO ANSWER VALUES (500, 'Betriebswirtschaftslehre', 1, 'true', 1003);
-INSERT INTO ANSWER VALUES (510, 'Informatik', 2, 'true', 1003);
-INSERT INTO ANSWER VALUES (600, 'Paris', 1, 'false', 1004);
-INSERT INTO ANSWER VALUES (700, 'New York', 2, 'false', 1004);
-INSERT INTO ANSWER VALUES (800, 'Peking', 3, 'true', 1004);
-INSERT INTO ANSWER VALUES (900, 'Elmshorn', 4, 'false', 1004);
-INSERT INTO ANSWER VALUES (1000, 'Programmieren', 1, 'true', 1005);
-INSERT INTO ANSWER VALUES (1100, 'Programmieren', 2, 'true', 1005);
-INSERT INTO ANSWER VALUES (1200, 'Software entwickeln', 3, 'true', 1005);
-INSERT INTO ANSWER VALUES (1300, 'Geschaeftsprozesse modellieren', 4, 'true', 1005);
-INSERT INTO ANSWER VALUES (1400, 'echt leckeren Kuchen backen', 5, 'false', 1005);
+INSERT INTO ANSWER VALUES (10000, 'Softwaretechnik existiert seit 1969.', 1, 'false', 100100);
+INSERT INTO ANSWER VALUES (20000, 'Das ist die richtige Antwort.', 2, 'true', 100100);
+INSERT INTO ANSWER VALUES (30000, 'Das ist die falsche Antwort.', 3, 'true', 100100);
+INSERT INTO ANSWER VALUES (40000, 'richtige', 1, 'true', 100200);
+INSERT INTO ANSWER VALUES (50000, 'Betriebswirtschaftslehre', 1, 'true', 100300);
+INSERT INTO ANSWER VALUES (51000, 'Informatik', 2, 'true', 100300);
+INSERT INTO ANSWER VALUES (60000, 'Paris', 1, 'false', 100400);
+INSERT INTO ANSWER VALUES (70000, 'New York', 2, 'false', 100400);
+INSERT INTO ANSWER VALUES (80000, 'Peking', 3, 'true', 100400);
+INSERT INTO ANSWER VALUES (90000, 'Elmshorn', 4, 'false', 100400);
+INSERT INTO ANSWER VALUES (100000, 'Programmieren', 1, 'true', 100500);
+INSERT INTO ANSWER VALUES (110000, 'Programmieren', 2, 'true', 100500);
+INSERT INTO ANSWER VALUES (120000, 'Software entwickeln', 3, 'true', 100500);
+INSERT INTO ANSWER VALUES (130000, 'Geschaeftsprozesse modellieren', 4, 'true', 100500);
+INSERT INTO ANSWER VALUES (140000, 'echt leckeren Kuchen backen', 5, 'false', 100500);
 
-INSERT INTO PARTICIPATION VALUES (1, 'a', TRUE,1234, 1234);
-INSERT INTO PARTICIPATION VALUES (2, '40e02bb7-860b-46e4-aa19-39088bf57d9a', TRUE,1235, 1235);
+INSERT INTO PARTICIPATION VALUES (6, 'a', TRUE,1234, 1234);
+INSERT INTO PARTICIPATION VALUES (5, '40e02bb7-860b-46e4-aa19-39088bf57d9a', TRUE,1235, 1235);
 
