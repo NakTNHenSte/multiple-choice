02e447a126ad833b67a4bcec3fbc8e1decf44856
DieLue <dieke_morten.luebberstedt@nordakademie.de>
2017-11-25 21:16:18 +0100

    fuer daniel Session.get

--- src/main/java/de/nordakademie/multiplechoice/exam/ui/ExamListAction.java
+++ src/main/java/de/nordakademie/multiplechoice/exam/ui/ExamListAction.java
@@ -3,16 +3,19 @@ package de.nordakademie.multiplechoice.exam.ui;
 import com.opensymphony.xwork2.Action;
 import de.nordakademie.multiplechoice.exam.model.Exam;
 import de.nordakademie.multiplechoice.exam.service.ExamService;
+import org.apache.struts2.interceptor.SessionAware;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.Map;
 
-public class ExamListAction implements Action {
+public class ExamListAction implements Action, SessionAware {
 
 
     private final ExamService examService;
     private Exam exam;
     private List<Exam> exams;
+    private Map<String, Object> sessionMap;
 
     @Autowired
     public ExamListAction(final ExamService examService) {
@@ -21,6 +24,7 @@ public class ExamListAction implements Action {
 
     @Override
     public String execute() {
+        if(sessionMap.get("userTyp") == "S")
         exams = examService.findAll();
         return SUCCESS;
     }
@@ -35,6 +39,10 @@ public class ExamListAction implements Action {
 
     public void setExam(Exam exam) {
         this.exam = exam;
+    }
+
+    public void setSession(Map<String, Object> sessionMap) {
+        this.sessionMap = sessionMap;
     }
 
 
