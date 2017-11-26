6a2d3327e2ca65f1f39f88ce452d154b9526b6a1
Christopher Ost <christopher.ost@nordakademie.de>
2017-11-26 17:52:02 +0100

    import.sql korrigiert

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
 INSERT INTO USER VALUES (123400, 'Hans', 'password', 'Petersen', 'S', 'hans.petersen');
-INSERT INTO USER VALUES (123500, 'Diecke', 'password', 'Luebberstadt', 'S', 'diecke.luebberstedt');
+INSERT INTO USER VALUES (123500, 'Diecke', 'password', 'Luebberstadt', 'S', 'diecke.luebberstadt');
 INSERT INTO USER VALUES (123600, 'Chris', 'password', 'West', 'S', 'chris.west');
 INSERT INTO USER VALUES (123700, 'Schtiewn', 'password', 'Hens', 'S', 'schtiewn.hens');
 INSERT INTO USER VALUES (123800, 'Daniela', 'password', 'Katzenburger', 'S', 'daniela.katzenburger');
